f92bba9e6e48a9054b84d1036660d7a06f12804a
Dan Daggett <dan.daggett@lacework.net>
2024-03-27 13:15:38 -0700

    Update example CTF infrastructure with example JSON object of challenge. Remove errant print statement.

--- ctf/1_getting to know you/__init__.py
+++ ctf/1_getting to know you/__init__.py
@@ -6,6 +6,20 @@ def parse_challenge(challenge, config, lw):
         This function will be ran on any challenge in this category at build time.
 
         * challenge - is a JSON representation of each individual challenge defined as YAML in challenges.yml
+          {
+              "name": "it's called vuln management, heard of it?",
+              "value": 5,
+              "type": "standard",
+              "state": "visible",
+              "description": "The CISO just agreed to present rolling updates on the new vulnerability management program to the board of directors over the next quarter. For the next update, the CISO wants to highlight a particular host that has been prioritized to be the top work item using Lacework data.  Which means, someone is now responsible for identifying the host that will be showcased in the CISO's next report. Any guesses on who that person is?  \n\nNavigate to the Host Vulnerabilities dossier and make sure you landed on the Host tab on the top of your screen. Filter the list of hosts to find the hostname that has the most vulnerabilities that are:  \n- Fixable  \n- Active package status  \n- Critical or High severity  \n",
+              "flags": [
+                  {
+                      "type": "static",
+                      "data": "case_insensitive",
+                      "content": "defiantly answering"
+                  }
+              ]
+          }
         * config - is a JSON representation of the supplied config.json when the build.py script was ran
         * lw - is a Lacework object that wraps the Lacework CLI and allows you to query data for the supplied profile in config
 
@@ -14,7 +28,6 @@ def parse_challenge(challenge, config, lw):
         
     '''
     if challenge['name'] == "it's called vuln management, heard of it?":
-        print(challenge)
         vuln_json = {
             "filters": [
                 {

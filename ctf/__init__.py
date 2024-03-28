db0a934edd69abec51b1a282865b474ec111b040
Dan Daggett <dan.daggett@lacework.net>
2024-03-28 10:25:41 -0700

    Simplify example of build_config provided with ctf example schema.

--- ctf/__init__.py
+++ ctf/__init__.py
@@ -3,24 +3,12 @@ from prompt_toolkit.shortcuts import checkboxlist_dialog
 from prompt_toolkit.shortcuts import input_dialog
 
 
-def choose_option(data, entity):
-    choices = []
-    results = ''
-    if entity == 'ctfd_url':
-        results = input_dialog(
-            title='Enter CTFd URL',
-            text='https://xxx.xxx.xxx.xxx:port').run()
-
-    elif entity == 'ctfd_api_key':
-        results = input_dialog(
-            password=True,
-            title='Enter CTFd API Key',
-            text='Provide API key from a CTFd admin account').run()
-
-    return results
-
-
 def build_config(config, lw):
-    config['ctfd_url'] = choose_option('', 'ctfd_url')
-    config['ctfd_api_key'] = choose_option('', 'ctfd_api_key')
+    config['ctfd_url'] = input_dialog(
+        title='Enter CTFd URL',
+        text='https://xxx.xxx.xxx.xxx:port').run()
+    config['ctfd_api_key'] = input_dialog(
+        password=True,
+        title='Enter CTFd API Key',
+        text='Provide API key from a CTFd admin account').run()
     return config
